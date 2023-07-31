e521ee9456e19d62b8fe6affa34edbb8d21ed3a2
Emiliano Rotta <emilianorottaf@gmail.com>
2023-07-31 13:04:37 -0300

    nuevos ejercicios hechos

--- index.js
+++ index.js
@@ -48,11 +48,75 @@
 
 // DOM
 
-var etiqueta = document.querySelector("h1")
-var etiquetaTitulo = document.querySelector(".titulo")
-var etiquetaId = document.querySelector("#id")
+// var etiqueta = document.querySelector("h1")
+// var etiquetaTitulo = document.querySelector(".titulo")
+// var etiquetaId = document.querySelector("#id")
 
 
-etiqueta.innerHTML = "despedida"
-etiquetaTitulo.style.color = "red"
-etiquetaId.innerHTML = "otra despedida"
+// etiqueta.innerHTML = "despedida"
+// etiquetaTitulo.style.color = "red"
+// etiquetaId.innerHTML = "otra despedida"
+
+
+// 2 ejercicio Heladería
+
+// Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada".
+
+// var gustoUno = prompt("primer gusto")
+// var gustoDos = prompt("segundo gusto")
+// var gustoTres = prompt("tercer gusto")
+
+// alert("Aquí tiene su helado de " + gustoUno + ", " + gustoDos + " y " + gustoTres)
+
+// 3 Ejercicio Datos personales
+
+// Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos.
+
+// var nombre = prompt("ingrese su nombre")
+// var apellido = prompt("ingrese su apellido")
+// var edad = prompt("ingrese su edad")
+// var nacionalidad = prompt("ingrese su nacionalidad")
+// var documento = prompt("ingrese su documento")
+
+
+
+// alert("Nuevo usuario agregado al sistema: " + nombre + ", " + apellido + ", " + edad + ", " + nacionalidad + " y " + documento)
+
+// Años perro
+
+// Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro.
+
+// var edad = prompt("escriba la edad")
+
+// var resultado = edad * 7
+
+// alert("la edad ingresada es: " + edad + " y la edad perro es: "+ resultado)
+
+
+// Días a segundos
+
+// Crear un programa que pida ingresar una cantidad de días y muestre un mensaje con el resultado de la conversión en segundos.
+
+// opcion 1
+
+// var dias = prompt("ingrese numero de dia")
+
+// var horas = dias * 24
+// var minutos = horas * 60
+// var segundos = minutos * 60
+
+// alert("los dias ingresados fueron: " + dias + " y los segundos son: " + segundos)
+
+// //opcion 2
+// var segundo = dias * 86400
+// alert("los dias ingresados fueron: " + dias + " y los segundos son: " + segundo)
+
+
+// Área de un triangulo
+
+// Crear un programa que pida al usuario ingresar el valor de la base y el valor de la altura de un triangulo, calcular su área y mostrarlo en un mensaje.
+
+// var base = prompt("escribir la base")
+// var altura = prompt("escribir la altura")
+
+// alert("el area es: " + base * altura)
